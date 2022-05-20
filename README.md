1e135017dd0f50276860c9fe875b0db4d6c66277
rochakravi <rochak.ravi@gmail.com>
2022-05-20 14:19:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-epwxqr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-epwxqr)
---
+++ src/app/my-service.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MyServiceService {
+  // https://jsonplaceholder.typicode.com/todos
+  // https://jsonplaceholder.typicode.com/users
+  constructor() {}
+}
